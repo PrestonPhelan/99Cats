9e2a5b290bbd815acdf721430532d9833b13634a
PrestonPhelan <preston.phelan@gmail.com>
2017-07-17 14:49:34 -0700

    Age function in Cat model

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -4,4 +4,8 @@ class Cat < ApplicationRecord
   validates :birth_date, :color, :name, :sex, presence: true
   validates :sex, inclusion: %w(M F)
   validates :color, inclusion: COLORS
+
+  def age
+    (Date.today - birth_date).to_i / 365
+  end
 end
