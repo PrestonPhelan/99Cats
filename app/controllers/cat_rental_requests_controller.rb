a92cf640c3bb3f8f1551acc90866cead4acec13d
PrestonPhelan <preston.phelan@gmail.com>
2017-07-18 14:31:29 -0700

    New cat rental request form

---
+++ app/controllers/cat_rental_requests_controller.rb
@@ -0,0 +1,17 @@
+class CatRentalRequestsController < ApplicationController
+  def create
+    @request = CatRentalRequest.create(request_params)
+    if @request.save
+      redirect_to cat_url(@request.cat)
+    else
+      flash.now[:errors] = @request.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def request_params
+    params.require[:cat_rental_request].permit[:cat_id, :start_date, :end_date, :status]
+  end
+end
