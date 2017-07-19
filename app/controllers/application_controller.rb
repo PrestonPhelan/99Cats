a12b375d28089979074088724c9c492f43072f8f
PrestonPhelan <preston.phelan@gmail.com>
2017-07-19 11:58:24 -0700

    Session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 
+  helper_method :current_user
+
   def current_user
     @current_user ||= User.find_by_session_token(session[:session_token])
   end
@@ -8,5 +10,10 @@ class ApplicationController < ActionController::Base
   def login(user)
     session[:session_token] = user.reset_session_token!
     @current_user = user
+  end
+
+  def logout
+    @current_user.reset_session_token!
+    session[:session_token] = nil
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:user_name],
+      params[:user][:password]
+    )
+    if @user
+      login(@user)
+      redirect_to cats_url
+    else
+      flash.now[:errors] = ["Invalid username/password"]
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+  end
 end
