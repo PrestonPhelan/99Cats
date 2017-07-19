bd1aa645119f0a1e3a90d0feb34223514fa7d22a
PrestonPhelan <preston.phelan@gmail.com>
2017-07-19 13:38:48 -0700

    User & cat associations, update create action

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,11 +1,14 @@
 class Cat < ApplicationRecord
   COLORS = %w(black white orange brown)
 
-  validates :birth_date, :color, :name, :sex, presence: true
+  validates :birth_date, :color, :name, :sex, :owner, presence: true
   validates :sex, inclusion: %w(M F)
   validates :color, inclusion: COLORS
 
   has_many :cat_rental_requests, dependent: :destroy
+  belongs_to :owner,
+    foreign_key: :user_id,
+    class_name: :user
 
   def age
     (Date.today - birth_date).to_i / 365
