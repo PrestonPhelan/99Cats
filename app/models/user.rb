4206287363a31cd09127c6be52b6be98eaaaef88
PrestonPhelan <preston.phelan@gmail.com>
2017-07-19 12:26:38 -0700

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   after_initialize :ensure_session_token
 
+  validates :user_name, :session_token, :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
   def self.find_by_credentials(username, password)
     @user = User.find_by_user_name(username)
     @user && @user.is_password?(password) ? @user : nil
